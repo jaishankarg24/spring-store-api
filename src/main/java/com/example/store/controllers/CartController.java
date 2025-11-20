555e8a073871f40c70877dc848408115d73e05be
jaishankarg24 <jaishankarg24@gmail.com>
2025-11-20 08:57:20 +0530

    Documenting APIs with Swagger

--- src/main/java/com/example/store/controllers/CartController.java
+++ src/main/java/com/example/store/controllers/CartController.java
@@ -7,6 +7,9 @@ import com.example.store.dtos.UpdateCartItemRequest;
 import com.example.store.exceptions.CartNotFoundException;
 import com.example.store.exceptions.ProductNotFoundException;
 import com.example.store.services.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/carts")
+@Tag(name = "Carts")
 public class CartController {
     private final CartService cartService;
 
@@ -34,7 +38,9 @@ public class CartController {
     }
 
     @PostMapping("/{cartId}/items")
+    @Operation(summary = "Adds a product to the cart.")
     public ResponseEntity<CartItemDto> addToCart(
+            @Parameter(description = "The ID of the cart.")
             @PathVariable UUID cartId,
             @RequestBody AddItemToCartRequest request) {
 
